1603678bef2070cc11bfe742a7e867d9b383ad68
Paul Clifford <clifford@queensu.ca>
2018-09-21 16:08:34 -0400

    moved to react-slick slider, first draft

--- src/components/slide.js
+++ src/components/slide.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const Slide = () => {
-  return <div className="slide">
-    hi
+const Slide = ({ node }) => (
+  <div className="slide">
+
+    <figure className="slide-image">
+      <img 
+        src={node.featured_media.localFile.childImageSharp.resize.src}
+        alt={node.featured_media.alt_text}
+      />
+    </figure>
+
+    <main>
+      <h3 className="slide-title">{node.title}</h3>
+      <div className="slide-excerpt" dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+    </main>
+
   </div>
-}
+)
 
 export default Slide
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,55 +1,44 @@
 import React, { Component } from "react"
 import { graphql } from "gatsby"
 import Helmet from "react-helmet"
+import Slider from "react-slick"
+
 import Slide from "../components/slide"
 import "../styles/style.scss"
 
-class Slider extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      currentIndex: 0
-    }
-  }
-
-  goToPrevSlide = () => {
-    this.setState(prevState => ({
-      currentIndex: prevState.currentIndex - 1
-    }));
-    console.log(this.state.currentIndex)
-  }
-
-  goToNextSlide = () => {
-    this.setState(prevState => ({
-      currentIndex: prevState.currentIndex + 1
-    }));
-    console.log(this.state.currentIndex)
-  }
+class Home extends Component {
 
   render() {
     const data = this.props.data;
+
+    const settings = {
+      centerMode: true,
+      centerPadding: '0px',
+      fade: true,
+      slidesToShow: 1,
+      autoplay: true,
+      speed: 2000,
+      autoplaySpeed: 10000,
+      cssEase: "linear"
+    };
 
     return (
       <div className="slider">
         <Helmet htmlAttributes={{ lang: "en" }}>
           <meta charSet="utf-8" />
           <title>Digital Futures</title>
-        </Helmet> 
-
-        {data.allWordpressPost.edges.map(({ node }) => (
-          <Slide key={node.slug} node={node} />
-        ))}
-
-        <button onClick={this.goToPrevSlide}>Prev</button>
-        <button onClick={this.goToNextSlide}>Next</button>
-
+        </Helmet>
+        <Slider {...settings}>
+          {data.allWordpressPost.edges.map(({ node }) => (
+            <Slide key={node.slug} node={node} />
+          ))}
+        </Slider>
       </div>
     )
   }
 }
 
-export default Slider
+export default Home
 
 export const pageQuery = graphql`
 {
@@ -63,7 +52,7 @@ export const pageQuery = graphql`
           alt_text
           localFile {
             childImageSharp {
-              resize(width: 800) {
+              resize(width: 600, height: 600) {
                 src
               }
             }
