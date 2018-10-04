09ac40585b6f48bd7e69ebe4af56a91dd982e4cb
Paul Clifford <clifford@queensu.ca>
2018-10-04 14:15:05 -0400

    crop focus center

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,7 +52,7 @@ export const pageQuery = graphql`
             alt_text
             localFile {
               childImageSharp {
-                resize(width: 850, height: 850) {
+                resize(width: 850, height: 850, cropFocus: CENTER) {
                   src
                 }
               }
